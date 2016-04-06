c819ffa2e3c8ec937064eab343c38f8c8463305c
peterf89 <hustle21>
2016-04-07 01:29:54 +0200

    Added code to check connection to server. Still sloppy/messy, will do cleanup soon.

--- app/src/main/java/nl/windesheim/capturetheclue/MainActivity.java
+++ app/src/main/java/nl/windesheim/capturetheclue/MainActivity.java
@@ -3,13 +3,25 @@ package nl.windesheim.capturetheclue;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import nl.windesheim.capturetheclue.Connection.JSONParser;
+import nl.windesheim.capturetheclue.Connection.Server;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String serverStatus = "Not connected";
+    public static Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = (TextView) findViewById(R.id.status);
+        b = (Button) findViewById(R.id.settings);
+        b.setText(serverStatus);
+        tv.setText(serverStatus);
     }
 
     public void onClickMain(View view) {
@@ -25,7 +37,8 @@ public class MainActivity extends AppCompatActivity {
 
             case R.id.settings:
                 //
-                break;
+
+                new Server().testConnection();
         }
     }
 }
