b2f642860a8e818ec3e1ab7d04afb5696be1a08b
kyuhuh <kyuisgoodforyou@gmail.com>
2016-05-25 11:19:06 +0200

    wordselec2/camera ongoing

--- app/src/main/java/nl/windesheim/capturetheclue/GameFirstActivity.java
+++ app/src/main/java/nl/windesheim/capturetheclue/GameFirstActivity.java
@@ -1,38 +1,186 @@
 package nl.windesheim.capturetheclue;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameFirstActivity extends AppCompatActivity {
 
     //show the word on the top
 
-    /*
-    ImageView imageView;
-    File outputFile;
-*/
+
+//    ImageView imageView;
+//    File outputFile;
+    public static TextView selectedWordDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
-        /*
-        imageView = (ImageView) findViewById(R.id.imageView);
-        File storageDir = Environment.getExternalStorageDirectory();
-        outputFile = new File(storageDir, "output.jpg");
-        */
+        setContentView(R.layout.activity_gamefirst);
 
-        //error : save after crashed
+        selectedWordDisplay = (TextView)findViewById(R.id.selectedWordDisplay);
+//        String username = settings.getString("username", "not_found");
+//        userNameDisplay.setText(username);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String value = extras.getString("selectedWord");
+            Log.d("debug",value);
+            selectedWordDisplay.setText(value);
+        }
+
+        //ADD CAMERA FUNCTIE
+        //SEND DATA TO SERVER
+
+//        new PostDataAsyncTask().execute();
     }
+
+
+
+    //post text or file to the server
+//
+//    public class PostDataAsyncTask extends AsyncTask<String, String, String> {
+//
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//            // do stuff before posting data
+//        }
+//
+//        @Override
+//        protected String doInBackground(String... strings) {
+//            try {
+//                // 1 = post text data, 2 = post file
+//                int actionChoice = 2;
+//
+//                // post a text data
+//                if(actionChoice==2){
+//                    postFile();
+//                } else {
+//                    postText();
+//                }
+//
+//            } catch (NullPointerException e) {
+//                e.printStackTrace();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String lenghtOfFile) {
+//            // do stuff after posting data
+//        }
+//    }
+//
+//    private void postText(){
+//        //post text to the server
+//        try{
+//            // url where the data will be posted
+//            String postReceiverUrl = "http://patatjes.esy.es/post.php";
+//            Log.d("debug", "postURL: " + postReceiverUrl);
+//
+//            // HttpClient
+//            HttpClient httpClient = new DefaultHttpClient();
+//
+//            // post header
+//            HttpPost httpPost = new HttpPost(postReceiverUrl);
+//
+//            // add your data
+//            List<NameValuePair> selectedWords = new ArrayList<NameValuePair>(2);
+//            selectedWords.add(new BasicNameValuePair("word", "WORD"));
+//
+//            httpPost.setEntity(new UrlEncodedFormEntity(selectedWords));
+//
+//            // execute HTTP post request
+//            HttpResponse response = httpClient.execute(httpPost);
+//            HttpEntity resEntity = response.getEntity();
+//
+//            if (resEntity != null) {
+//
+//                String responseStr = EntityUtils.toString(resEntity).trim();
+//                Log.v("debug", "Response: " +  responseStr);
+//
+//                // you can add an if statement here and do other actions based on the response
+//            }
+//
+//        }  catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    // will post our text file
+//    private void postFile(){
+//        try{
+//
+//            // the file to be posted
+//            String textFile = Environment.getExternalStorageDirectory() + "/sample.txt";
+//            Log.d("debug", "textFile: " + textFile);
+//
+//            // the URL where the file will be posted
+//            String postReceiverUrl = "http://patatjes.esy.es/post.php";
+//            Log.d("debug", "postURL: " + postReceiverUrl);
+//
+//            // new HttpClient
+//            HttpClient httpClient = new DefaultHttpClient();
+//
+//            // post header
+//            HttpPost httpPost = new HttpPost(postReceiverUrl);
+//
+//            File file = new File(textFile);
+//            FileBody fileBody = new FileBody(file);
+//
+//            MultipartEntity reqEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+//            reqEntity.addPart("file", fileBody);
+//            httpPost.setEntity(reqEntity);
+//
+//            // execute HTTP post request
+//            HttpResponse response = httpClient.execute(httpPost);
+//            HttpEntity resEntity = response.getEntity();
+//
+//            if (resEntity != null) {
+//
+//                String responseStr = EntityUtils.toString(resEntity).trim();
+//                Log.d("debug", "Response: " +  responseStr);
+//
+//                // you can add an if statement here and do other actions based on the response
+//            }
+//
+//        } catch (NullPointerException e) {
+//            e.printStackTrace();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 }
--- app/src/main/java/nl/windesheim/capturetheclue/WordselectionActivity.java
+++ app/src/main/java/nl/windesheim/capturetheclue/WordselectionActivity.java
@@ -1,34 +1,52 @@
 package nl.windesheim.capturetheclue;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class WordSelectionActivity extends AppCompatActivity {
 
     String myJSON;
 
     private static final String TAG_RESULTS = "result";
-    private static final String TAG_ID = "id";
     private static final String TAG_WORD = "word";
 
-    JSONArray words = null;
+    JSONArray words;
 
     ArrayList<HashMap<String, String>> wordList;
 
@@ -44,23 +62,17 @@ public class WordSelectionActivity extends AppCompatActivity {
         getData("http://patatjes.esy.es/words.php");
     }
 
-
-
     protected void showList() {
         try {
             JSONObject jsonObj = new JSONObject(myJSON);
             words = jsonObj.getJSONArray(TAG_RESULTS);
 
-
-
             for (int i = 0; i < words.length(); i++) {
                 JSONObject c = words.getJSONObject(i);
-                String id = c.getString(TAG_ID);
                 String word = c.getString(TAG_WORD);
 
                 HashMap<String, String> words = new HashMap<String, String>();
 
-                words.put(TAG_ID,id);
                 words.put(TAG_WORD, word);
 
                 wordList.add(words);
@@ -68,16 +80,29 @@ public class WordSelectionActivity extends AppCompatActivity {
 
             ListAdapter adapter = new SimpleAdapter(
                     WordSelectionActivity.this, wordList, R.layout.list_item,
-                    new String[]{TAG_ID,TAG_WORD},
-                    new int[]{R.id.id,R.id.word}
+                    new String[]{TAG_WORD},
+                    new int[]{R.id.word}
             );
 
             list.setAdapter(adapter);
 
+            //onClick
+            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    String selectedWord = list.getItemAtPosition(position).toString();
+                    Log.d("debug", selectedWord);
+
+                    Intent gameStartIntent = new Intent(getApplicationContext(), GameFirstActivity.class);
+                    gameStartIntent.putExtra("selectedWord",selectedWord);
+                    startActivity(gameStartIntent);
+                }
+            });
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
     }
 
     public void getData(String url) {
@@ -118,4 +143,82 @@ public class WordSelectionActivity extends AppCompatActivity {
         GetDataJSON g = new GetDataJSON();
         g.execute(url);
     }
+
+
+
+
+
+    // post text to server
+
+    /*
+    public class PostDataAsyncTask extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // do stuff before posting data
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                // 1 = post text data, 2 = post file
+                int actionChoice = 2;
+
+                // post a text data
+                if(actionChoice==1){
+                    postText();
+                }
+
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String lenghtOfFile) {
+            // do stuff after posting data
+        }
+    }
+
+    // this will post our text data
+    private void postText(){
+        try{
+            // url where the data will be posted
+            String postReceiverUrl = "http://patatjes.esy.es/post.php";
+            Log.d("debug", "postURL: " + postReceiverUrl);
+
+            // HttpClient
+            HttpClient httpClient = new DefaultHttpClient();
+
+            // post header
+            HttpPost httpPost = new HttpPost(postReceiverUrl);
+
+            // add your data
+            List<NameValuePair> selectedWords = new ArrayList<NameValuePair>(2);
+            selectedWords.add(new BasicNameValuePair("word", "Mike"));
+
+            httpPost.setEntity(new UrlEncodedFormEntity(selectedWords));
+
+            // execute HTTP post request
+            HttpResponse response = httpClient.execute(httpPost);
+            HttpEntity resEntity = response.getEntity();
+
+            if (resEntity != null) {
+
+                String responseStr = EntityUtils.toString(resEntity).trim();
+                Log.v("debug", "Response: " +  responseStr);
+
+                // you can add an if statement here and do other actions based on the response
+            }
+
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    */
 }
